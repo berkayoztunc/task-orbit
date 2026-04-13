1a5671ad5faad7c3284bb10809b2dee8bf20033c
Huseyin Kizilcik <kzlckhuseyin@gmail.com>
2026-04-14 01:32:07 +0300

    User model updated, API route added.

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\InternshipController;
 use App\Http\Controllers\ProfileController;
 
 Route::get('/user', function (Request $request) {
@@ -20,5 +21,6 @@ Route::get('/test', function () {
 Route::apiResource('companies', CompanyController::class);
 //profiles
 Route::apiResource('profiles', ProfileController::class);
+Route::apiResource('internships', InternshipController::class);
 Route::get('companies/{id}/profiles', [CompanyController::class, 'profiles']);
 Route::get('companies/{id}/internships', [CompanyController::class, 'internships']);
